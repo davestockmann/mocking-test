76c74480d4085eebb7ab725921ccc584c29731e3
Dave Stockmann <dave.stockmann@perficient.com>
2016-11-11 14:30:02 -0600

    added another test which checks for exceptions

--- src/main/java/com/perficient/product/service/PricingServiceImpl.java
+++ src/main/java/com/perficient/product/service/PricingServiceImpl.java
@@ -5,6 +5,11 @@ import java.math.BigDecimal;
 public class PricingServiceImpl {
 
 	public BigDecimal getPrice(Long productId) {
+		
+		if (productId == null) {
+			throw new IllegalArgumentException("productId can not be null");
+		}
+		
 		return new BigDecimal("25.00");
 	}
 
--- src/test/java/com/perficient/product/service/PricingServiceImplTest.java
+++ src/test/java/com/perficient/product/service/PricingServiceImplTest.java
@@ -13,24 +13,35 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PricingServiceImplTest {
-	
+
 	@Mock
 	private PricingServiceImpl priceServiceMock;
-	
+
 	@Test
 	public void findProduct() {
 		Long productId = Long.getLong("39933");
 		BigDecimal expectedPrice = new BigDecimal(199.99);
-		
+
 		ArgumentCaptor<Long> productIdCaptor = ArgumentCaptor.forClass(Long.class);
 		when(priceServiceMock.getPrice(productIdCaptor.capture())).thenReturn(expectedPrice);
-		
-		
+
 		ProductServiceImpl productService = new ProductServiceImpl(priceServiceMock);
 		BigDecimal price = productService.findPrice(productId);
-		
+
 		Assertions.assertThat(price).isEqualTo(expectedPrice);
 		Assertions.assertThat(productIdCaptor.getValue()).isEqualTo(productId);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void findProduct_ExpectsException() {
+		Long productId = null;
+		BigDecimal expectedPrice = new BigDecimal(199.99);
+
+		when(priceServiceMock.getPrice(productId)).thenThrow(IllegalArgumentException.class);
+
+		ProductServiceImpl productService = new ProductServiceImpl(priceServiceMock);
+		BigDecimal price = productService.findPrice(productId);
+
 	}
 
 }
